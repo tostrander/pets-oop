b2a00ceb6a2cf57bb5a48dadc910b1fdb928b6b1
Tina Ostrander <tostrander@greenriver.edu>
2019-05-06 17:09:21 -0400

    Constructor added

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php
 
 //Turn on error reporting
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
 
 //Require the autoload file
 require_once('vendor/autoload.php');
-require('pet.php');
+
+//If you start a session, do if AFTER
+//requiring the autoload
+session_start();
 
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
 //Define a default route
 $f3->route('GET /',
     function($f3) {
+
         //Instantiate the Pet class
         $pet1 = new Pet();
         $f3->set('pet1', $pet1);
+
+        $pet2 = new Pet("Fido", "pink");
+        $f3->set('pet2', $pet2);
+
+        $pet3 = new Pet("Rufus");
+        $f3->set('pet3', $pet3);
 
         $template = new Template();
         echo $template->render('views/my-pets.html');
